ac89ada1d676770af77a1dd74512f68bc5cff252
voti <kayzcrack1@yahoo.com>
2015-09-01 16:49:42 +0100

    added script videoupload.py that opens a file, read its content and update the database

---
+++ videoupload.py
@@ -0,0 +1,29 @@
+import csv, sqlite3
+import os,sys
+import shutil
+
+user_input = raw_input("Enter the path of your video: ")
+# open a file
+#thisfile = os.open(user_input+"/manifest.txt",os.O_RDWR)
+# reading text
+#output = os.read(thisfile,100)
+#print output
+
+#source = user_input #video source
+#designated_dest = "/video"
+#shutil.move(source, designated_dest)  
+
+conn = sqlite3.connect("test.db")
+curs = conn.cursor()
+curs.execute('''DROP TABLE PCFC''')
+curs.execute("CREATE TABLE PCFC (id INTEGER PRIMARY KEY, type INTEGER, term TEXT, definition TEXT);")
+reader = csv.reader(open(user_input+'/manifest.txt', 'r'), delimiter='|')
+for row in reader:
+    to_db = [unicode(row[0], "utf8"), unicode(row[1], "utf8"), unicode(row[2], "utf8")]
+    curs.execute("INSERT INTO PCFC (type, term, definition) VALUES (?, ?, ?);", to_db)
+conn.commit()
+
+meta = curs.execute("select * from PCFC")
+for r in meta:
+    print r
+    
