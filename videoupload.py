23997f4399b29d7930a2207b4dd02e42cb6eac81
voti <kayzcrack1@yahoo.com>
2015-09-02 09:02:18 +0100

    modified videoupload.py file, changed promt message when script is executed

--- videoupload.py
+++ videoupload.py
@@ -2,7 +2,7 @@ import csv, sqlite3
 import os,sys
 import shutil
 
-user_input = raw_input("Enter the path of your video: ")
+user_input = raw_input("Enter the path to copy video from: ")
 # open a file
 #thisfile = os.open(user_input+"/manifest.txt",os.O_RDWR)
 # reading text
@@ -17,13 +17,13 @@ conn = sqlite3.connect("test.db")
 curs = conn.cursor()
 curs.execute('''DROP TABLE PCFC''')
 curs.execute("CREATE TABLE PCFC (id INTEGER PRIMARY KEY, type INTEGER, term TEXT, definition TEXT);")
-reader = csv.reader(open(user_input+'/manifest.txt', 'r'), delimiter='|')
+reader = csv.reader(open(user_input+'/manifest.txt', 'r'), delimiter=',')
 for row in reader:
     to_db = [unicode(row[0], "utf8"), unicode(row[1], "utf8"), unicode(row[2], "utf8")]
     curs.execute("INSERT INTO PCFC (type, term, definition) VALUES (?, ?, ?);", to_db)
 conn.commit()
 
-meta = curs.execute("select * from PCFC")
-for r in meta:
-    print r
+data = curs.execute("select * from PCFC")
+for rel in data:
+    print rel
     
